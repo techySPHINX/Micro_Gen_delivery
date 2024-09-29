8c8b0758b0a17a51d49a674831b0f1a92684b9b5
techySPHINX <shimon123universe@gmail.com>
2024-09-29 13:56:30 +0530

    feat: Intial user setUp

---
+++ servers/apps/gateways/src/app.module.ts
@@ -0,0 +1,26 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users',
+              url: 'http://localhost:4001/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ servers/apps/users/src/users.module.ts
@@ -0,0 +1,27 @@
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { JwtService } from '@nestjs/jwt'
+import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, ConfigService, JwtService],
+})
+export class UsersModule {}
